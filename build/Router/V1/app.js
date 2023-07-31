83b1f2c2b009db7befc4e1b9cb7abe7e3ac7b042
muhammed danish <muhammeddanishu@gmail.com>
2023-07-31 10:18:49 +0530

    upgrade

--- build/Router/V1/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Routerv1 = void 0;
-var express_1 = require("express");
-var app = (0, express_1.Router)();
-app.get('/', function (req, res) {
-    res.json({ status: true, message: "v111" });
-});
-exports.Routerv1 = app;
